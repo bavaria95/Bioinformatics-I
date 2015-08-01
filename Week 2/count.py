af027ee7de4db350aa97b644b70cb6c865e838d5
Dmitry Petruk <bavaria95@gmail.com>
2015-08-01 17:47:48 +0300

    approx count

--- Week 2/count.py
+++ Week 2/count.py
@@ -1,3 +1,16 @@
 def hamdist(str1, str2):
 	return len(list(filter(lambda x : x[0] != x[1], zip(str1, str2))))
 
+def approx_pattern_count(genome, pattern, d):
+	count = 0
+	for i in range(len(genome) - len(pattern) + 1):
+		if hamdist(genome[i : i + len(pattern)], pattern) <= d:
+			count += 1
+
+	return count
+
+pattern = input()
+genome = input()
+d = int(input())
+
+print(approx_pattern_count(genome, pattern, d))
