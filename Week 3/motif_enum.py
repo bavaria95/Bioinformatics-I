ed0fcff8bf6c7aa7769a6ee230075bb5ee7d3e6c
Dmitry Petruk <bavaria95@gmail.com>
2015-08-09 18:53:52 +0300

    pattern matching in genome with d mismatches

--- Week 3/motif_enum.py
+++ Week 3/motif_enum.py
@@ -1,3 +1,6 @@
+import sys
+import itertools
+
 def neighbors(pattern, d):
 	alph = 'ACGT'   # alphabet
 	a = list(range(len(pattern)))
@@ -19,3 +22,16 @@ def neighbors(pattern, d):
 				neighbors_words.append(tmp)
 
 	return neighbors_words
+
+def hamdist(str1, str2):
+	return len(list(filter(lambda x : x[0] != x[1], zip(str1, str2))))
+
+def approx_pattern_matching(pattern, genome, d):
+	k = len(pattern)
+	matches = []
+
+	for i in range(len(genome) - k + 1):
+		if hamdist(genome[i : i + k], pattern) <= d :
+			matches.append(i)
+
+	return matches
