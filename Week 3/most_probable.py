309bd0feeb91cdd56c274d61add42d414bd427c3
Dmitry Petruk <bavaria95@gmail.com>
2015-08-10 16:22:34 +0300

    calculating probability of occuring single pattern with given probabilities of single nucleotide

--- Week 3/most_probable.py
+++ Week 3/most_probable.py
@@ -5,6 +5,18 @@ def probability(pattern, p):
 
 	return prob
 
+def most_probable(text, k, p):
+	max_prob = 0
+	most_probable_kmer = None
+
+	for i in range(len(text) - k + 1):
+		pattern = text[i : i + k]
+		prob = probability(pattern, p)
+		if max_prob < prob:
+			max_prob = prob
+			most_probable_kmer = pattern
+
+	return most_probable_kmer
 
 text = input()
 k = int(input())
@@ -14,3 +26,5 @@ p['A'] = list(map(float, input().split()))
 p['C'] = list(map(float, input().split()))
 p['G'] = list(map(float, input().split()))
 p['T'] = list(map(float, input().split()))
+
+print(most_probable(text, k, p))
