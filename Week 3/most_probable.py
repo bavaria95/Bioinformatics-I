8ffd489512ff39ef9a2d2d68616aab94cff110ae
Dmitry Petruk <bavaria95@gmail.com>
2015-08-10 16:04:41 +0300

    reading text, k and probabilities

---
+++ Week 3/most_probable.py
@@ -0,0 +1,7 @@
+text = input()
+k = int(input())
+
+A = list(map(float, input().split()))
+C = list(map(float, input().split()))
+G = list(map(float, input().split()))
+T = list(map(float, input().split()))
