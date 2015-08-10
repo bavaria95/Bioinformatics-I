31886756d872528f215a05a66ebfad91f30f48d8
Dmitry Petruk <bavaria95@gmail.com>
2015-08-10 16:14:26 +0300

    converting probabilities for single nucleotides into a dictionary

--- Week 3/most_probable.py
+++ Week 3/most_probable.py
@@ -1,12 +1,8 @@
-# def probability()
-
 text = input()
 k = int(input())
 
-A = list(map(float, input().split()))
-C = list(map(float, input().split()))
-G = list(map(float, input().split()))
-T = list(map(float, input().split()))
-
-p = [A, C, G, T]
-print(p)
+p = {}
+p['A'] = list(map(float, input().split()))
+p['C'] = list(map(float, input().split()))
+p['G'] = list(map(float, input().split()))
+p['T'] = list(map(float, input().split()))
